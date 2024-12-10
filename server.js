8c6194a5b438da2e879ce92325f9c405cefcc048
Erik Jacome <138533562+erikenrique@users.noreply.github.com>
2024-12-10 17:40:18 -0500

    testing cors issues

--- server.js
+++ server.js
@@ -33,18 +33,26 @@ const localServerPort = process.env.LOCAL_SERVER_PORT || 5000;
 const app = express();
 
 const allowedOrigins = [
-    process.env.PUBLIC_BASE_URL,
+    `https://live-gfx.onrender.com`,
     `http://${localIP}:${localServerPort}`,
     'http://localhost:5000',
-    'http://10.0.0.165:5000'
 ];
 
 // Configure CORS to allow the local server origin
 app.use(
     cors({
-        origin: `http://${localIP}:${localServerPort}`, // Allow only the local server origin
-        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Explicitly allow HTTP methods
-        credentials: true // Allow cookies and authorization headers
+        origin: function (origin, callback) {
+            console.log(`CORS request from origin: ${origin}`);
+
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                console.error(`CORS issue: Origin ${origin} not allowed.`);
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+        credentials: true,
     })
 );
 
